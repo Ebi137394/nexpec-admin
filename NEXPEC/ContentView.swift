57c6c41677c3079272536bd1826be1f2049e490d
Ebi <ebrahimfeyzi.ta@gmail.com>
2025-12-26 11:07:50 -0500

    Initial Commit

---
+++ NEXPEC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NEXPEC
+//
+//  Created by Ebrahim Feyzi on 2025-12-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NEXPEC/NEXPECApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEXPECApp.swift
+//  NEXPEC
+//
+//  Created by Ebrahim Feyzi on 2025-12-26.
+//
+
+import SwiftUI
+
+@main
+struct NEXPECApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
